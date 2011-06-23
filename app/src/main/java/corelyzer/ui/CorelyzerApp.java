f9d004e140364b510af8de95a3d529117a6b2a0a
Josh Reed <obitus9982@gmail.com>
2011-06-22 19:32:38 -0500

    Fixed up the Mac app bundle building

--- app/src/main/java/corelyzer/ui/CorelyzerApp.java
+++ app/src/main/java/corelyzer/ui/CorelyzerApp.java
@@ -4,21 +4,21 @@
  * Copyright (C) 2004, 2005 Arun Gangadhar Gudur Rao, Julian Yu-Chung Chen
  * Electronic Visualization Laboratory, University of Illinois at Chicago
  *
- * This software is free software; you can redistribute it and/or modify it 
+ * This software is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either Version 2.1 of the License, or 
+ * the Free Software Foundation; either Version 2.1 of the License, or
  * (at your option) any later version.
  *
  * This software is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser Public License along
- * with this software; if not, write to the Free Software Foundation, Inc., 
+ * with this software; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * Questions or comments about CoreWall should be directed to 
+ * Questions or comments about CoreWall should be directed to
  * cavern@evl.uic.edu
  *
  *****************************************************************************/
@@ -309,9 +309,9 @@ public class CorelyzerApp extends WindowAdapter implements MouseListener, Startu
 	 * Entry point of the application. Initialized the application, starts up
 	 * the corelyzer.helper.SceneGraph system, and setups up last known
 	 * settings.
-	 * 
+	 *
 	 * @param args
-	 *            Plugin names and input session file, if there's any
+	 *			  Plugin names and input session file, if there's any
 	 */
 	public static void main(final String[] args) {
 		CRPreferences prefs = handlePreferences();
@@ -325,7 +325,7 @@ public class CorelyzerApp extends WindowAdapter implements MouseListener, Startu
 		/*
 		 * if (prefs.getUseQuaqua()) {
 		 * System.setProperty("Quaqua.tabLayoutPolicy", "wrap");
-		 * 
+		 *
 		 * try { UIManager.setLookAndFeel(
 		 * "ch.randelshofer.quaqua.QuaquaLookAndFeel"); } catch (Exception e) {
 		 * System.err.println("Exception in setting LAF: " + e); } }
@@ -735,7 +735,7 @@ public class CorelyzerApp extends WindowAdapter implements MouseListener, Startu
 
 	/**
 	 * Returns the vertical DPI of all the screens.
-	 * 
+	 *
 	 * @return float
 	 */
 	public float getScreenDpiY() {
@@ -1798,7 +1798,7 @@ public class CorelyzerApp extends WindowAdapter implements MouseListener, Startu
 
 		// Create Help Menu
 		// Create JavaHelp hooks
-		File helpSet = new File("../help/jhelpset.hs");
+		File helpSet = new File("help/jhelpset.hs");
 		HelpSet hs = null;
 		try {
 			URL hsURL = helpSet.toURI().toURL();
