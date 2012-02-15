5449dd14ad7faa1e065121fc10f38dfd3f95f2db
Brian Grivna <sorghumking@yahoo.com>
2012-02-14 18:27:12 -0600

    [Forgot to include in previous commit]

--- scenegraph/src/main/java/corelyzer/graphics/SceneGraph.java
+++ scenegraph/src/main/java/corelyzer/graphics/SceneGraph.java
@@ -1765,6 +1765,7 @@ public class SceneGraph {
 
 	public native static boolean trackIsStaggered(int track);
 	public native static void staggerTrackSections(int track, boolean stagger);
+	public native static void trimSections(int track, float trim);
 	
 	/**
 	 * Let's the corelyzer.helper.SceneGraph library know that it's being
