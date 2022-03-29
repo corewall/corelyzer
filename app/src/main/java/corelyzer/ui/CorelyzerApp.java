ed3245f438b9b84bcdf69dfe18470697a1d5479d
Brian G <sorghumking@yahoo.com>
2022-03-29 15:23:25 -0500

    Version to 2.1.3

--- app/src/main/java/corelyzer/ui/CorelyzerApp.java
+++ app/src/main/java/corelyzer/ui/CorelyzerApp.java
@@ -923,7 +923,7 @@ public class CorelyzerApp extends WindowAdapter implements MouseListener, Startu
 
 	public String getCorelyzerVersion()
 	{
-		String defaultVersion = "2.1.2";
+		String defaultVersion = "2.1.3";
 		String implVersion = this.getClass().getPackage().getImplementationVersion();
 		return (implVersion == null ? defaultVersion : implVersion);
 		// return defaultVersion;
